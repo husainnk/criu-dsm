3a68927ca72175abda46544d22dc062080af9e52
Mohamed Husain <nkhusain@vt.edu>
2023-06-21 09:57:09 -0400

    Added restorer script

---
+++ restorer.sh
@@ -0,0 +1,14 @@
+while [ 1 ] ;
+do
+	node_id=$1
+	app=`cat /tmp/cmd` ;
+
+	echo "Restoring application '$app' at remote_node $node_id"
+
+	cd ~/$app;
+	rm  -r *.img ;
+	cp ../aarch64/*.img . ;
+
+	python3 ~/thread_migrater/migrater.py $node_id ;
+	sudo ~/criu-dsm/criu-3.15/criu/criu  restore -vvv --shell-job;
+done
