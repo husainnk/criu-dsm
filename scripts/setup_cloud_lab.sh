4111fad4786d0f31ab3f4312d7e0bb9c0031ebab
Mohamed Husain <nkhusain@vt.edu>
2023-06-21 02:47:43 -0400

    Add cloud lab setup scripts

---
+++ scripts/setup_cloud_lab.sh
@@ -0,0 +1,56 @@
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    echo "Usage:"
+    echo "$0 <host>"
+    exit 0
+fi
+
+CHOST=$1
+BRANCH=$2
+
+scp -o  StrictHostKeyChecking=no ./oh_my_zsh_install.sh ${CHOST}:
+
+ssh -o StrictHostKeyChecking=no ${CHOST}  <<'ENDSSH'
+
+
+	$(declare -p  BRANCH)
+	echo "Branch $BRANCH"
+
+	yes '' | ssh-keygen -N '' > /dev/null
+	sudo apt update
+	sudo apt install -y zsh libprotobuf-dev libprotobuf-c-dev protobuf-c-compiler protobuf-compiler python-protobuf \
+	pkg-config libnl-3-dev libnet-dev libcap-dev libbsd-dev python3-pip cmake
+    	sudo apt  install make libncurses-dev git g++ python3 flex  bison bc  build-essential cmake   libedit-dev python zlib1g-dev rsync wget libelf-dev libssl-dev dwarves
+	python3 -m pip install pyelftools jsonpath-ng pyro4 psutil scp protobuf==3.20 capstone keystone keystone-engine  ip-address
+
+	git clone https://github.com/husainnk/thread_migrater.git
+	git clone https://github.com/husainnk/criu-dsm.git -b $BRANCH
+	make -C criu-dsm/criu-3.15/  -j8
+	make -C criu-dsm/criu-3.15/dsm_client
+
+	chmod +x ./oh_my_zsh_install.sh ; ./oh_my_zsh_install.sh
+	git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+	echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"   >> ~/.zshrc
+	echo 'export PS1="%(?:%{%}%{$fg[green]%}%n@%{$fg[red]%}%m ➜ :%{%}➜ ) %{$fg[cyan]%}%d%{$reset_color%}[cloud-lab] "'   >> ~/.zshrc
+
+
+	### setup kernel
+	#sudo dpkg -i /proj/popcornlinux-PG0/mohamed/kernel_images_wp/x86_images/*.deb
+	#sudo rm -rv /boot/*5.4.0-100*
+	#sudo ln -sf /boot/initrd.img-5.14.0-custom /boot/initrd.img
+	#sudo ln -sf /boot/vmlinuz-5.14.0-custom /boot/vmlinuz
+	#sudo update-grub
+
+	echo "set hlsearch" >> ~/.vimrc
+	echo "set number" >> ~/.vimrc
+	echo "set noincsearch" >> ~/.vimrc
+
+	sync
+
+ENDSSH
+
+exit
+
+
