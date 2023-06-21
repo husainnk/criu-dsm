f3e5b978c06a933f4687f080a9b0937de8492dc3
Mohamed Husain <nkhusain@vt.edu>
2023-06-21 08:50:55 -0400

    Update scripts: setup and scp

--- scripts/dsm_scp.sh
+++ scripts/dsm_scp.sh
@@ -19,33 +19,42 @@ cd ~/${app}
 #time sudo python3 /users/nkhusain/TransProc_c/criu-3.15/crit/crit  recode . ./aarch64/ aarch64 ${app} ./bin/ y 
 
 sudo rm -r aarch64/ ; mkdir aarch64; cp *.img  aarch64/
+
+#### Remote Node 1
 ssh ${host} -C "rm -r ~/aarch64; sudo killall -9 ${app}" ;  
 scp -r ./aarch64 ${host}:
-ssh ${2} -C "echo 1 > /tmp/cmd"
+ssh ${2} -C "echo ${app} > /tmp/cmd"
 
 
 if [ -z "${3}" ];
 then
 	exit
 fi
+
+#### Remote Node 2
 echo "========Transfer to  3rd Node====="
 ssh $3 -C "rm -r ~/aarch64/*.img; sudo killall -9 $app; mkdir ~/aarch64/" ; 
 scp  -r *.img ${3}:~/aarch64/
-ssh ${3} -C "echo 1 > /tmp/cmd"
+ssh ${3} -C "echo $app > /tmp/cmd"
 
 if [ -z "${4}" ];
 then
 	exit
 fi
+
+### Remote Node 3
+
 ssh $4 -C "rm -r ~/aarch64/*.img; sudo killall -9 $app; mkdir ~/aarch64/" ; 
 scp  -r *.img ${4}:~/aarch64/
-ssh ${4} -C "echo 1 > /tmp/cmd"
+ssh ${4} -C "echo $app > /tmp/cmd"
 
 if [ -z "${5}" ];
 then
 	exit
 fi
 
+
+
 ssh $5 -C "rm -r ~/aarch64/*.img; sudo killall -9 $app; mkdir ~/aarch64/" ; 
 scp  -r *.img ${5}:~/aarch64/
-ssh ${5} -C "echo 1 > /tmp/cmd"
+ssh ${5} -C "echo $app > /tmp/cmd"
--- scripts/setup_cloud_lab.sh
+++ scripts/setup_cloud_lab.sh
@@ -8,11 +8,23 @@ if [ $# -eq 0 ]
 fi
 
 CHOST=$1
+
+
+if [ "$2" == "--setup-kernel" ] ;  then
+	ssh -o StrictHostKeyChecking=no ${CHOST}  -C "sudo dpkg -i /proj/popcornlinux-PG0/mohamed/kernel_images_wp/x86_images/*.deb"
+	ssh -o StrictHostKeyChecking=no ${CHOST}  -C "sudo rm -rv /boot/*5.4.0-100*"
+	ssh -o StrictHostKeyChecking=no ${CHOST}  -C "sudo ln -sf /boot/initrd.img-5.14.0-custom /boot/initrd.img"
+	ssh -o StrictHostKeyChecking=no ${CHOST}  -C "sudo ln -sf /boot/vmlinuz-5.14.0-custom /boot/vmlinuz"
+	ssh -o StrictHostKeyChecking=no ${CHOST}  -C "sudo update-grub"
+	echo ""
+	echo "Setup done: Reboot the device"
+	exit;
+fi
+
+
 BRANCH=$2
-
 scp -o  StrictHostKeyChecking=no ./oh_my_zsh_install.sh ${CHOST}:
-
-ssh -o StrictHostKeyChecking=no ${CHOST}  <<'ENDSSH'
+ssh -o StrictHostKeyChecking=no ${CHOST}  'bash -s'  <<EOT
 
 
 	$(declare -p  BRANCH)
@@ -21,27 +33,27 @@ ssh -o StrictHostKeyChecking=no ${CHOST}  <<'ENDSSH'
 	yes '' | ssh-keygen -N '' > /dev/null
 	sudo apt update
 	sudo apt install -y zsh libprotobuf-dev libprotobuf-c-dev protobuf-c-compiler protobuf-compiler python-protobuf \
-	pkg-config libnl-3-dev libnet-dev libcap-dev libbsd-dev python3-pip cmake
-    	sudo apt  install make libncurses-dev git g++ python3 flex  bison bc  build-essential cmake   libedit-dev python zlib1g-dev rsync wget libelf-dev libssl-dev dwarves
+	pkg-config libnl-3-dev libnet-dev libcap-dev libbsd-dev python3-pip cmake make libncurses-dev git g++ python3 flex  bison bc  build-essential cmake   libedit-dev python zlib1g-dev rsync wget libelf-dev libssl-dev dwarves
 	python3 -m pip install pyelftools jsonpath-ng pyro4 psutil scp protobuf==3.20 capstone keystone keystone-engine  ip-address
 
 	git clone https://github.com/husainnk/thread_migrater.git
 	git clone https://github.com/husainnk/criu-dsm.git -b $BRANCH
 	make -C criu-dsm/criu-3.15/  -j8
-	make -C criu-dsm/criu-3.15/dsm_client
+	if [ "$BRANCH" == "dsm_client" ] ; then
+		make -C criu-dsm/criu-3.15/dsm_client
+	fi
 
 	chmod +x ./oh_my_zsh_install.sh ; ./oh_my_zsh_install.sh
 	git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 	echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"   >> ~/.zshrc
-	echo 'export PS1="%(?:%{%}%{$fg[green]%}%n@%{$fg[red]%}%m ➜ :%{%}➜ ) %{$fg[cyan]%}%d%{$reset_color%}[cloud-lab] "'   >> ~/.zshrc
+	echo 'export PS1="%(?:%{%}%{%}%n@%{%}%m ➜ :%{%}➜ ) %{%}%d%{%}[cloud-lab] "'   >> ~/.zshrc
 
-
-	### setup kernel
-	#sudo dpkg -i /proj/popcornlinux-PG0/mohamed/kernel_images_wp/x86_images/*.deb
-	#sudo rm -rv /boot/*5.4.0-100*
-	#sudo ln -sf /boot/initrd.img-5.14.0-custom /boot/initrd.img
-	#sudo ln -sf /boot/vmlinuz-5.14.0-custom /boot/vmlinuz
-	#sudo update-grub
+#	### setup kernel
+#	sudo dpkg -i /proj/popcornlinux-PG0/mohamed/kernel_images_wp/x86_images/*.deb
+#	sudo rm -rv /boot/*5.4.0-100*
+#	sudo ln -sf /boot/initrd.img-5.14.0-custom /boot/initrd.img
+#	sudo ln -sf /boot/vmlinuz-5.14.0-custom /boot/vmlinuz
+#	sudo update-grub
 
 	echo "set hlsearch" >> ~/.vimrc
 	echo "set number" >> ~/.vimrc
@@ -49,7 +61,7 @@ ssh -o StrictHostKeyChecking=no ${CHOST}  <<'ENDSSH'
 
 	sync
 
-ENDSSH
+EOT
 
 exit
 
